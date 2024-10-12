0ee856a34923d7b2d5ff82e2569ae1903d2a60a0
Emil Majchrzak <majchrzamemil@github.com>
2024-10-12 13:50:35 +0200

    handle GPIO in separate thread

--- doorbell/src/main.rs
+++ doorbell/src/main.rs
@@ -1,10 +1,13 @@
 use axum::extract::ws::Message;
+use axum::extract::State;
 use axum::{extract::ConnectInfo, response::IntoResponse, routing::any, Router};
 use rppal::gpio::{Gpio, Level};
 use std::error::Error;
 use std::net::SocketAddr;
-use std::thread;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::time::Duration;
+use std::{io, thread};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use tower_http::trace::{DefaultMakeSpan, TraceLayer};
@@ -14,6 +17,11 @@ const GPIO_SENSOR: u8 = 23;
 //API definition
 //[0] - doorbell
 //[1] - error
+
+#[derive(Clone)]
+struct AppState {
+    gpio_state: Arc<AtomicBool>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -25,48 +33,80 @@ async fn main() -> Result<(), Box<dyn Error>> {
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
+
+    let gpio_state = Arc::new(AtomicBool::new(false));
+    let state = AppState {
+        gpio_state: gpio_state.clone(),
+    };
     let app = Router::new()
         .route("/doorbell", any(ws_handler))
+        .with_state(state)
         .layer(TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::default()));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     tracing::debug!("listening on {}", listener.local_addr()?);
 
-    Ok(axum::serve(
-        listener,
-        app.into_make_service_with_connect_info::<SocketAddr>(),
-    )
-    .await?)
+    futures::future::try_join_all(vec![
+        tokio::spawn(async move {
+            axum::serve(
+                listener,
+                app.into_make_service_with_connect_info::<SocketAddr>(),
+            )
+            .await
+        }),
+        tokio::spawn(async move { gpio_hander(gpio_state.clone()) }),
+    ])
+    .await?;
+    Ok(())
+}
+
+fn gpio_hander(state: Arc<AtomicBool>) -> Result<(), io::Error> {
+    let Ok(gpio) = Gpio::new() else {
+        tracing::error!("Unable to init GPIO");
+        return Ok(());
+    };
+
+    let Ok(pin) = gpio.get(GPIO_SENSOR) else {
+        tracing::error!("Unable to get GPIO pin {GPIO_SENSOR}");
+        return Ok(());
+    };
+    let pin = pin.into_input_pullup();
+    loop {
+        if pin.read() == Level::Low {
+            if !state.load(Ordering::Acquire) {
+                state.store(true, Ordering::Release);
+                tracing::info!("State updated to true");
+            }
+        } else {
+            if state.load(Ordering::Acquire) {
+                state.store(false, Ordering::Release);
+                tracing::info!("State updated to false");
+            }
+        }
+        thread::sleep(Duration::from_millis(500));
+    }
 }
 
 async fn ws_handler(
     ws: axum::extract::WebSocketUpgrade,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    State(state): State<AppState>,
 ) -> impl IntoResponse {
     tracing::info!("WS handler");
-    let upgrade = ws.on_upgrade(move |socket| handle_socket(socket, addr));
+    let upgrade = ws.on_upgrade(move |socket| handle_socket(socket, addr, state.gpio_state));
     tracing::info!("upgrade finished");
     upgrade
 }
 
-async fn handle_socket(mut socket: axum::extract::ws::WebSocket, who: SocketAddr) {
+async fn handle_socket(
+    mut socket: axum::extract::ws::WebSocket,
+    who: SocketAddr,
+    gpio_state: Arc<AtomicBool>,
+) {
     tracing::info!("Connected client: {who}");
 
-    let Ok(gpio) = Gpio::new() else {
-        tracing::error!("Unable to init GPIO");
-        send_message(&mut socket, Message::Binary(vec![1])).await;
-        return;
-    };
-
-    let Ok(pin) = gpio.get(GPIO_SENSOR) else {
-        tracing::error!("Unable to get GPIO pin {GPIO_SENSOR}");
-        send_message(&mut socket, Message::Binary(vec![1])).await;
-        return;
-    };
-    let pin = pin.into_input_pullup();
-
     loop {
-        if pin.read() == Level::Low {
+        if gpio_state.load(Ordering::Acquire) {
             tracing::info!("doorbell");
             if send_message(&mut socket, Message::Binary(vec![0]))
                 .await

f426a64b1f5ae974b5790a87ead63b4de92f20c6
Emil Majchrzak <majchrzamemil@github.com>
2024-10-12 12:36:09 +0200

    improved observability

--- doorbell/src/main.rs
+++ doorbell/src/main.rs
@@ -5,15 +5,29 @@ use std::error::Error;
 use std::net::SocketAddr;
 use std::thread;
 use std::time::Duration;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
 // Gpio uses BCM pin numbering. BCM GPIO 24 is tied to physical pin 16.
 const GPIO_SENSOR: u8 = 23;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("initing doorbel");
-    let app = Router::new().route("/doorbell", any(ws_handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let app = Router::new()
+        .route("/doorbell", any(ws_handler))
+        .layer(TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::default()));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    tracing::debug!("listening on {}", listener.local_addr()?);
+
     Ok(axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
@@ -28,22 +42,22 @@ async fn ws_handler(
     ws.on_upgrade(move |socket| handle_socket(socket, addr))
 }
 async fn handle_socket(mut socket: axum::extract::ws::WebSocket, who: SocketAddr) {
-    println!("Connected client: {who}");
+    tracing::info!("Connected client: {who}");
     // Send error message on invalid init
     let gpio = Gpio::new().unwrap();
     let pin = gpio.get(GPIO_SENSOR).unwrap().into_input_pulldown();
     loop {
         if pin.read() == Level::Low {
             match socket.send(Message::Binary(vec![])).await {
-                Ok(_) => println!("doorbell"),
+                Ok(_) => tracing::info!("doorbell"),
                 Err(e) => {
-                    eprintln!("Encountered error:{e}");
+                    tracing::error!(err = ?e, "Encountered error");
                     break;
                 }
             }
-            thread::sleep(Duration::from_millis(2000));
+            thread::sleep(Duration::from_millis(2500));
         } else {
-            thread::sleep(Duration::from_millis(300));
+            thread::sleep(Duration::from_millis(500));
         }
     }
     println!("Closing connection with: {who}");
